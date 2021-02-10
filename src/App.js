2ae32c6a72dc5bfd302eef0a4493712d2d617c69
TALBI Abdelbari <abdelbari1996@hotmail.com>
2021-02-10 22:29:31 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Konva from "konva";
+import { Stage, Layer, Rect, Text } from "react-konva";
+
+class ColoredRect extends React.Component {
+  state = {
+    color: "green"
+  };
+  handleClick = () => {
+    this.setState({
+      color: Konva.Util.getRandomColor()
+    });
+  };
+  render() {
+    return (
+      <Rect
+        x={20}
+        y={20}
+        width={50}
+        height={50}
+        fill={this.state.color}
+        shadowBlur={5}
+        onClick={this.handleClick}
+      />
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Text text="Try click on rect" />
+          <ColoredRect />
+        </Layer>
+      </Stage>
+    );
+  }
+}
+
+export default App;
